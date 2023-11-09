46e508b21fd884e0a0817b972f9327dc8b674f82
Fumiya Sasaki <fumiya.sasaki.9933@gmail.com>
2023-11-09 23:39:39 +0900

    リファクタリング

--- macroGs.js
+++ macroGs.js
@@ -3,6 +3,21 @@ const menuItems = ['main', 'drink', 'sweets'];
 const formatDayTime = (value, format) => Utilities.formatDate(value,
     SpreadsheetApp.getActiveSpreadsheet().getSpreadsheetTimeZone(), format);
 
+const formatAlphabet = (number) => String.fromCodePoint(number);
+
+const createInitialPrace = (items) => [...Array(items.length)].map(() => 0);
+
+const createInitialEmptyData = (items) => [...Array(items.length - 2)].map(item => '');
+
+const getInsertDate = (day, time) => formatDayTime(day, 'YYYY/MM/dd') + ' ' + formatDayTime(time, 'hh:mm');
+
+const getSumPrace = (items, rowNumber) => items.length > 0 ? '=' + items.map(item =>
+    `(${item[2]}*$${formatAlphabet(item[1])}$${rowNumber})`).join('+') : 0;
+
+const writeDetailPeople = (rowNNumber) => `=IFERROR(VLOOKUP($B$${rowNNumber},'名簿'!$B$2:$F$103,2,false),'該当者無')`;
+
+const setBorderStyle = (func) => func.setBorder(true, true, true, true, true, true, 'black', SpreadsheetApp.BorderStyle.SOLID);
+
 const setListDetail = () => {
     const spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
     // const activeSheet = spreadsheet.getActiveSheet();
@@ -29,42 +44,31 @@ const setListDetail = () => {
             countAlphabet++;
         };
     });
-    const createInitialPrace = (items) => [...Array(items.length)].map(() => 0);
-    const createInitialEmptyData = (items) => [...Array(items.length - 2)].map(item => '');
-    const getInsertDate = (day, time) => formatDayTime(day, 'YYYY/MM/dd') + ' ' + formatDayTime(time, 'hh:mm')
-    const getSumMainPrace = (rowNumber) => menuList.main.length > 0 ? menuList.main.map((item, i) =>
-        `(${item[2]}*$${String.fromCodePoint(item[1])}$${rowNumber})`).join('+') : 0;
 
-    const getSumDrinkPrace = (rowNumber) => menuList.drink.length > 0 ? menuList.drink.map((item, i) =>
-        `(${item[2]}*$${String.fromCodePoint(item[1])}$${rowNumber})`).join('+') : 0;
+    const getOnlySumSweetsPrace = (rowNumber) => menuList.sweets.length > 0 ? '=' + menuList.sweets.map((item, i) =>
+        `(${item[2]}*$${formatAlphabet(item[1] - (menuList.main.length + menuList.drink.length + 1))}$${rowNumber})`).join('+') : 0;
 
-    const getSumSweetsPrace = (rowNumber) => menuList.sweets.length > 0 ? menuList.sweets.map((item, i) =>
-        `(${item[2]}*$${String.fromCodePoint(item[1])}$${rowNumber})`).join('+') : 0;
-
-    const getONlySumSweetsPrace = (rowNumber) => menuList.sweets.length > 0 ? menuList.sweets.map((item, i) =>
-        `(${item[2]}*$${String.fromCodePoint(item[1] - (menuList.main.length + menuList.drink.length + 1))}$${rowNumber})`).join('+') : 0;
-
-    const getSumIndividualPrace = (rowNumber) => `SUM($${String.fromCodePoint(startAlphabet + headerMenuItems.length)}$${rowNumber}:
-    $${String.fromCodePoint(startAlphabet + headerMenuItems.length + 2)}$${rowNumber})`;
+    const getSumIndividualPrace = (rowNumber) => `=SUM($${formatAlphabet(startAlphabet + headerMenuItems.length)}$${rowNumber}:
+      $${formatAlphabet(startAlphabet + headerMenuItems.length + 2)}$${rowNumber})`;
 
     const headerMenuList = ['誰', 'リピート', '備考', ...headerMenuItems, 'メイン合計', 'ドリンク合計', 'お菓子合計', '個人合計', '部合計'];
-    const totalPartsAlphabet = String.fromCodePoint(startAlphabet + headerMenuList.length - 5);
+    const totalPartsAlphabet = formatAlphabet(startAlphabet + headerMenuList.length - 5);
     // 日付が入るので初期値に1行加算
     let targetRowNumberForList = startRowNumber + 1;
     const totalParts = `=SUM($${totalPartsAlphabet}$${targetRowNumberForList + 1}:$${totalPartsAlphabet}$${targetRowNumberForList + 6})`;
     const insertDataForSixPeople = [];
-    days.forEach((day, dayIndex) => {
-        times.forEach((time, timeIndex) => {
+    days.forEach(day => {
+        times.forEach(time => {
             const insertDate = [getInsertDate(day, time), ...createInitialEmptyData(headerMenuList), totalParts];
             const initialList = [...Array(6)].map((item, i) => [
                 '',
-                `=IFERROR(VLOOKUP($B$${targetRowNumberForList + 1 + i},'名簿'!$B$2:$F$103,2,false),'該当者無')`,
-                `=IFERROR(VLOOKUP($B$${targetRowNumberForList + 1 + i},'名簿'!$B$2:$F$103,2,false),'該当者無')`,
+                writeDetailPeople(targetRowNumberForList + 1 + i),
+                writeDetailPeople(targetRowNumberForList + 1 + i),
                 ...createInitialPrace(headerMenuItems),
-                '=' + getSumMainPrace(targetRowNumberForList + 1 + i),
-                '=' + getSumDrinkPrace(targetRowNumberForList + 1 + i),
-                '=' + getSumSweetsPrace(targetRowNumberForList + 1 + i),
-                '=' + getSumIndividualPrace(targetRowNumberForList + 1 + i),
+                getSumPrace(menuList.main, targetRowNumberForList + 1 + i),
+                getSumPrace(menuList.drink, targetRowNumberForList + 1 + i),
+                getSumPrace(menuList.sweets, targetRowNumberForList + 1 + i),
+                getSumIndividualPrace(targetRowNumberForList + 1 + i),
                 '']);
             initialList.unshift(insertDate);
             insertDataForSixPeople.push(initialList);
@@ -72,23 +76,30 @@ const setListDetail = () => {
         });
     });
     const insertMenuList = [headerMenuList, ...insertDataForSixPeople.flat()];
-    activeSheet.getRange(startRowNumber, 2, insertMenuList.length, headerMenuList.length).setValues(insertMenuList);
+
+    setBorderStyle(activeSheet.getRange(startRowNumber, 2, insertMenuList.length, headerMenuList.length).setValues(insertMenuList))
+    activeSheet.getRange(`$B$${startRowNumber}:$${formatAlphabet(65 + headerMenuList.length)}$${startRowNumber}`).setBackground('yellow');
+
+    for (let i = 1; i < insertDataForSixPeople.flat().length; i += 7) {
+        activeSheet.getRange(`$B$${startRowNumber + i}:$${formatAlphabet(65 + headerMenuList.length)}$${startRowNumber + i}`).setBackground('orange');
+    };
 
     const headerSweetsList = ['誰', '備考', ...menuList.sweets.map(item => item[0]), '個人合計', '部合計'];
     // 2行空けるため
     targetRowNumberForList += 2
-    const totalPartsSweetsAlphabet = String.fromCodePoint(startAlphabet + menuList.sweets.length - 1);
+    const totalPartsSweetsStartRow = targetRowNumberForList;
+    const totalPartsSweetsAlphabet = formatAlphabet(startAlphabet + menuList.sweets.length - 1);
 
-    const totalSweetsParts = `=SUM($${totalPartsSweetsAlphabet}$${targetRowNumberForList + 2}:$${totalPartsSweetsAlphabet}$${targetRowNumberForList + 6})`;
-    const insertDataSweetsForSixPeople = [];
-    days.forEach((day, dayIndex) => {
-        times.forEach((time, timeIndex) => {
-            const insertDateSweets = [getInsertDate(dat, time), ...createInitialEmptyData(headerSweetsList), totalSweetsParts];
+    let insertDataSweetsForSixPeople = [];
+    days.forEach(day => {
+        times.forEach(time => {
+            const totalSweetsParts = `=SUM($${totalPartsSweetsAlphabet}$${targetRowNumberForList + 2}:$${totalPartsSweetsAlphabet}$${targetRowNumberForList + 6})`;
+            const insertDateSweets = [getInsertDate(day, time), ...createInitialEmptyData(headerSweetsList), totalSweetsParts];
             const initialListSweets = [...Array(5)].map((item, i) => [
                 '',
                 '',
                 ...createInitialPrace(menuList.sweets),
-                '=' + getONlySumSweetsPrace(targetRowNumberForList + 2 + i),
+                getOnlySumSweetsPrace(targetRowNumberForList + 2 + i),
                 '']);
             initialListSweets.unshift(insertDateSweets);
             insertDataSweetsForSixPeople.push(initialListSweets);
@@ -96,7 +107,30 @@ const setListDetail = () => {
         });
     });
 
-    const insertSweetsData = [headerSweetsList, ...insertDataSweetsForSixPeople.flat()];
-    activeSheet.getRange(startRowNumber + insertMenuList.length + 2, 2, insertSweetsData.length,
-        headerSweetsList.length).setValues(insertSweetsData);
+    insertDataSweetsForSixPeople = [...insertDataSweetsForSixPeople.flat()];
+
+    const insertSweetsData = [headerSweetsList, ...insertDataSweetsForSixPeople];
+    const startRowFoeSweets = startRowNumber + insertMenuList.length + 2;
+    const lastAlphabetForSweets = formatAlphabet(65 + headerSweetsList.length);
+
+    setBorderStyle(activeSheet.getRange(startRowFoeSweets, 2, insertSweetsData.length, headerSweetsList.length).setValues(insertSweetsData));
+    activeSheet.getRange(`$B$${startRowFoeSweets}:$${lastAlphabetForSweets}$${startRowFoeSweets}`).setBackground('yellow');
+
+    for (let i = 1; i < insertDataSweetsForSixPeople.length; i += 6) {
+        activeSheet.getRange(`$B$${startRowFoeSweets + i}:$${lastAlphabetForSweets}$${startRowFoeSweets + i}`).setBackground('orange');
+    };
+
+    const sumMainPrace = startAlphabet + headerMenuItems.length;
+    const sumDrinkPrace = startAlphabet + headerMenuItems.length + 1;
+    const sumSweetsPrace = startAlphabet + headerMenuItems.length + 2;
+
+    const writeTotalPrace = (targetPrace) =>
+        `=SUM($${formatAlphabet(targetPrace)}$${startRowNumber}:$${formatAlphabet(targetPrace)}$${startRowNumber + insertMenuList.length - 1})`;
+
+    const startAlphabetForOnlySweets = ormatAlphabet(startAlphabet + menuList.sweets.length);
+
+    activeSheet.getRange(6, 3).setValue(writeTotalPrace(sumMainPrace));
+    activeSheet.getRange(7, 3).setValue(writeTotalPrace(sumDrinkPrace));
+    activeSheet.getRange(8, 3).setValue(writeTotalPrace(sumSweetsPrace)`
+    + SUM($${startAlphabetForOnlySweets}$${totalPartsSweetsStartRow + 1}:$${startAlphabetForOnlySweets}$${totalPartsSweetsStartRow + insertDataSweetsForSixPeople.length})`);
 };
